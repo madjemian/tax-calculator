cb4f0a54b6b550cd08774b1be47cdb900ceb8a68
Matthew Adjemian <madjemian@netflix.com>
2025-08-03 16:44:28 -0400

    TabbedUserInputs migrated

--- src/components/tabs/TabbedUserInputs.tsx
+++ src/components/tabs/TabbedUserInputs.tsx
@@ -1,4 +1,4 @@
-import { Tab } from 'semantic-ui-react'
+import { Tabs, Tab } from '@blueprintjs/core'
 import { observer } from 'mobx-react-lite'
 import { useContext } from 'react'
 import { userInputStoreContext } from '../../stores/stores'
@@ -10,26 +10,14 @@ import TaxesPaidTab from './TaxesPaidTab'
 const TabbedUserInputs = observer(() => {
   const userInputStore = useContext(userInputStoreContext)
 
-  const panes = [
-    {
-      menuItem: 'W2 Income',
-      render: () => <W2IncomeTab store={userInputStore} />,
-    },
-    {
-      menuItem: 'Investment Income',
-      render: () => <InvestmentIncomeTab store={userInputStore} />,
-    },
-    {
-      menuItem: 'Income Deductions',
-      render: () => <DeductionsTab store={userInputStore} />,
-    },
-    {
-      menuItem: 'Taxes Paid',
-      render: () => <TaxesPaidTab store={userInputStore} />,
-    },
-  ]
-
-  return <Tab panes={panes} />
+  return (
+    <Tabs id="user-input-tabs" defaultSelectedTabId="w2-income">
+      <Tab id="w2-income" title="W2 Income" panel={<W2IncomeTab store={userInputStore} />} />
+      <Tab id="investment-income" title="Investment Income" panel={<InvestmentIncomeTab store={userInputStore} />} />
+      <Tab id="deductions" title="Income Deductions" panel={<DeductionsTab store={userInputStore} />} />
+      <Tab id="taxes-paid" title="Taxes Paid" panel={<TaxesPaidTab store={userInputStore} />} />
+    </Tabs>
+  )
 })
 
 export default TabbedUserInputs
