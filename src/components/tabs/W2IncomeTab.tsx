6e373dc8e22be42ff41dacd4073282e6d8ed225b
Matthew Adjemian <madjemian@netflix.com>
2025-08-03 16:41:59 -0400

    W2Income migrated

--- src/components/tabs/W2IncomeTab.tsx
+++ src/components/tabs/W2IncomeTab.tsx
@@ -1,4 +1,5 @@
-import { Form, FormGroup, Header, Segment, SegmentGroup, Button, Input } from 'semantic-ui-react'
+import { Card, H2, H3, FormGroup, Button, InputGroup } from '@blueprintjs/core'
+import { Trash, Plus } from '@blueprintjs/icons'
 import { observer } from 'mobx-react-lite'
 import NumberInput from '../NumberInput'
 import type { UserInputStore } from '../../stores/UserInputStore'
@@ -8,115 +9,100 @@ const W2IncomeTab = observer((props: { store: UserInputStore }) => {
   
   return (
     <>
-      <Header as='h2' content='W2 Income' />
-      <Form widths='equal'>
-        <SegmentGroup>
-          {store.w2Income.map((w2) => (
-            <Segment key={w2.id}>
-              <FormGroup>
-                <Form.Field width={4}>
-                  <label>Name</label>
-                  <Input 
-                    value={w2.name} 
-                    onChange={(e) => store.updateW2Income(w2.id, { name: e.target.value })}
-                  />
-                </Form.Field>
-                <Form.Field width={4}>
-                  <label>Income</label>
-                  <NumberInput 
-                    value={w2.income} 
-                    changeFunction={(value) => store.updateW2Income(w2.id, { income: value })} 
-                  />
-                </Form.Field>
-                <Form.Field width={4}>
-                  <label>Withholding</label>
-                  <NumberInput 
-                    value={w2.withholding} 
-                    changeFunction={(value) => store.updateW2Income(w2.id, { withholding: value })} 
-                  />
-                </Form.Field>
-                <Form.Field width={4}>
-                  <label>&nbsp;</label>
-                  <Button 
-                    color='red' 
-                    icon='trash' 
-                    onClick={() => store.removeW2Income(w2.id)}
-                    disabled={store.w2Income.length <= 1}
-                  />
-                </Form.Field>
+      <H2>W2 Income</H2>
+      <div>
+        {store.w2Income.map((w2) => (
+          <Card key={w2.id} style={{ marginBottom: '16px' }}>
+            <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap', alignItems: 'end' }}>
+              <FormGroup label="Name" style={{ minWidth: '200px' }}>
+                <InputGroup 
+                  value={w2.name} 
+                  onChange={(e) => store.updateW2Income(w2.id, { name: e.target.value })}
+                />
               </FormGroup>
-            </Segment>
-          ))}
-          
-          <Segment>
-            <Button 
-              color='green' 
-              icon='plus' 
-              content='Add W2' 
-              onClick={() => store.addW2Income()}
-            />
-          </Segment>
-
-          <Segment>
-            <Header as='h3' content='Option Exercise Batches' />
-          </Segment>
-
-          {store.optionExercises.map((option) => (
-            <Segment key={option.id}>
-              <FormGroup>
-                <Form.Field width={4}>
-                  <label>Date</label>
-                  <Input 
-                    type='date'
-                    value={option.date} 
-                    onChange={(e) => store.updateOptionExercise(option.id, { date: e.target.value })}
-                  />
-                </Form.Field>
-                <Form.Field width={4}>
-                  <label>Amount</label>
-                  <NumberInput 
-                    value={option.amount} 
-                    changeFunction={(value) => store.updateOptionExercise(option.id, { amount: value })} 
-                  />
-                </Form.Field>
-                <Form.Field width={4}>
-                  <label>Withholding</label>
-                  <NumberInput 
-                    value={option.withholding} 
-                    changeFunction={(value) => store.updateOptionExercise(option.id, { withholding: value })} 
-                  />
-                </Form.Field>
-                <Form.Field width={4}>
-                  <label>&nbsp;</label>
-                  <Button 
-                    color='red' 
-                    icon='trash' 
-                    onClick={() => store.removeOptionExercise(option.id)}
-                  />
-                </Form.Field>
+              <FormGroup label="Income" style={{ minWidth: '200px' }}>
+                <NumberInput 
+                  value={w2.income} 
+                  changeFunction={(value) => store.updateW2Income(w2.id, { income: value })} 
+                />
               </FormGroup>
-            </Segment>
-          ))}
+              <FormGroup label="Withholding" style={{ minWidth: '200px' }}>
+                <NumberInput 
+                  value={w2.withholding} 
+                  changeFunction={(value) => store.updateW2Income(w2.id, { withholding: value })} 
+                />
+              </FormGroup>
+              <div>
+                <Button 
+                  intent="danger"
+                  icon={<Trash />}
+                  onClick={() => store.removeW2Income(w2.id)}
+                  disabled={store.w2Income.length <= 1}
+                />
+              </div>
+            </div>
+          </Card>
+        ))}
+        
+        <Card style={{ marginBottom: '16px' }}>
+          <Button 
+            intent="success"
+            icon={<Plus />}
+            text="Add W2"
+            onClick={() => store.addW2Income()}
+          />
+        </Card>
 
-          <Segment>
-            <Button 
-              color='green' 
-              icon='plus' 
-              content='Add Option Exercise' 
-              onClick={() => store.addOptionExercise()}
-            />
-          </Segment>
-          
-          <Segment inverted color={'grey'}>
-            <FormGroup>
-              <Form.Field>
-                <label>Total W2 Income</label>
-                <NumberInput value={Math.round(store.totalW2Income)} />
-              </Form.Field>
-            </FormGroup>
-          </Segment>
-        </SegmentGroup>
-      </Form>
+        <H3>Option Exercise Batches</H3>
+
+        {store.optionExercises.map((option) => (
+          <Card key={option.id} style={{ marginBottom: '16px' }}>
+            <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap', alignItems: 'end' }}>
+              <FormGroup label="Date" style={{ minWidth: '200px' }}>
+                <InputGroup 
+                  type='date'
+                  value={option.date} 
+                  onChange={(e) => store.updateOptionExercise(option.id, { date: e.target.value })}
+                />
+              </FormGroup>
+              <FormGroup label="Amount" style={{ minWidth: '200px' }}>
+                <NumberInput 
+                  value={option.amount} 
+                  changeFunction={(value) => store.updateOptionExercise(option.id, { amount: value })} 
+                />
+              </FormGroup>
+              <FormGroup label="Withholding" style={{ minWidth: '200px' }}>
+                <NumberInput 
+                  value={option.withholding} 
+                  changeFunction={(value) => store.updateOptionExercise(option.id, { withholding: value })} 
+                />
+              </FormGroup>
+              <div>
+                <Button 
+                  intent="danger"
+                  icon={<Trash />}
+                  onClick={() => store.removeOptionExercise(option.id)}
+                />
+              </div>
+            </div>
+          </Card>
+        ))}
+
+        <Card style={{ marginBottom: '16px' }}>
+          <Button 
+            intent="success"
+            icon={<Plus />}
+            text="Add Option Exercise"
+            onClick={() => store.addOptionExercise()}
+          />
+        </Card>
+        
+        <Card style={{ backgroundColor: '#f5f5f5' }}>
+          <FormGroup label="Total W2 Income" style={{ minWidth: '200px' }}>
+            <NumberInput value={Math.round(store.totalW2Income)} />
+          </FormGroup>
+        </Card>
+      </div>
     </>
   )
 })
