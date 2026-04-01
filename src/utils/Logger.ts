5e3ec0015bf2912f49a4d667e26494191a7b2e17
madjemian <29743680+madjemian@users.noreply.github.com>
2026-04-01 17:26:20 -0400

    chore: lint error cleanup

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -3,30 +3,26 @@
  * Silences logs during test execution (NODE_ENV === 'test').
  */
 export const Logger = {
-  log: (...args: any[]) => {
+  log: (...args: unknown[]) => {
     if (process.env.NODE_ENV !== 'test') {
-      // eslint-disable-next-line no-console
       console.log(...args);
     }
   },
   
-  warn: (...args: any[]) => {
+  warn: (...args: unknown[]) => {
     if (process.env.NODE_ENV !== 'test') {
-      // eslint-disable-next-line no-console
       console.warn(...args);
     }
   },
 
-  error: (...args: any[]) => {
+  error: (...args: unknown[]) => {
     // We usually want to see errors even in tests, 
     // but you can wrap this if it becomes too noisy.
-    // eslint-disable-next-line no-console
     console.error(...args);
   },
 
-  info: (...args: any[]) => {
+  info: (...args: unknown[]) => {
     if (process.env.NODE_ENV !== 'test') {
-      // eslint-disable-next-line no-console
       console.info(...args);
     }
   }
